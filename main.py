ad18d738c44517c810a3e54f419c74191f667267
0000Kuzma <106026210+0000Kuzma@users.noreply.github.com>
2022-05-30 23:21:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher, executor
+
+bot = Bot(token='5335720245:AAHOQ2pJ3aStC0_5rIDsdoHRCFT18LTiXcg')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands="start")
+async def start(message):
+    await bot.send_message(message.chat.id, "Hi, im calculator, enter something")
+
+
+@dp.message_handler()
+async def start_command(message):
+    inp = message.text
+    try:
+        req = inp
+        if req == "":
+            return
+        # волшебный метод калькулятора) - eval
+
+        total = (str(eval(req)))
+        await bot.send_message(message.chat.id, total)
+    except (KeyboardInterrupt, EOFError, NameError, SyntaxError, TypeError):
+        await bot.send_message(message.chat.id, 'Нельзя использовать что-то')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
